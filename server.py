3029b3d48628aab352b89fd64343cd4c88a6e8f7
Aditya <aditya.jaiswal15974@sakec.ac.in>
2024-01-15 22:57:56 +0530

    json data

--- server.py
+++ server.py
@@ -7,16 +7,16 @@ app = Flask(__name__)
 def process_qr_code():
     data = decode('qr_code_data')
     if decoded_objects:
-        return decoded_objects[0].data.decode('utf-8')
+        return jsonify(decoded_objects[0].data.decode('utf-8'))
     else:
         return None
     # Your logic to process the QR code data goes here
     # For example, you can print it
-    print("QR Code Data:", data)
+    # print("QR Code Data:", data)
 
-    # Respond with some data back to the front end
-    response_data = {"message": "QR code data received successfully"}
-    return jsonify(response_data)
+    # # Respond with some data back to the front end
+    # response_data = {"message": "QR code data received successfully"}
+    # return jsonify(response_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
